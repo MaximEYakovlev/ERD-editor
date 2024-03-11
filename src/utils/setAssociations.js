5385e5ebc52af511456854e0119e9571a47e641b
Maxim Yakovlev <it.trainee@yandex.ru>
2024-03-11 22:29:20 +0300

    refactoring

--- src/utils/setAssociations.js
+++ src/utils/setAssociations.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import Xarrow from 'react-xarrows';
 
 export const setAssociations = (associate) => {
-    const associations = associate.map((association, index) => {
+    const associations = associate.map((association) => {
         const { start, end } = association;
 
         return <Xarrow key={uuidv4()} start={start} end={end} />;
